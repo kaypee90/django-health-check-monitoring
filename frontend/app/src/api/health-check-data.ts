71c8da4d28b32083d8d7a78d7e10412ab8aef9aa
kaypee90 <kaypee90@yahoo.com>
2024-02-12 00:56:13 +0000

    Remove frontend and backend

--- frontend/app/src/api/health-check-data.ts
+++
@@ -1,8 +0,0 @@
-import { HealthCheckCount } from '../models/index'
-import { instance } from './api-client'
-
-export type HealthCheckCountResponse = {
-    data: HealthCheckCount[]
-}
-
-export const getHealthCheckCountData = (): Promise<HealthCheckCountResponse> => instance.get('/healthcheckjobs')
--- monitoring/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from monitoring.views import HealthCheckJobView, health_check
-
-urlpatterns = [
-    path("_ping/", health_check),
-    path("healthcheckjobs/", HealthCheckJobView.as_view(), name="healthcheckjobs"),
-]
